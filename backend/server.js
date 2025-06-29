e0fb431682d209606e2e9f7b9c41826a7084fd29
spookyoo <melvincruz07@gmail.com>
2025-06-29 13:55:25 -0600

    database implementation
    - installed dotenv and mysql packages
    - database connection
    - champion and matchup table creation

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 "use strict";
 
+require("dotenv").config();
+
 const cors = require("cors");
 const express = require("express");
+const sqldb = require("mysql2");
 
 const PORT = 3000;
 const HOST = "0.0.0.0";
@@ -10,6 +13,55 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const db = sqldb.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+// CHECK DB CONNECTION, EXIT IF ERROR
+db.connect((err) => {
+    if (err) {
+        console.error("ERROR CONNECTING TO DB", err);
+        process.exit(1);
+    }
+    console.log("CONNECTED TO DB");
+    createTables(db)
+});
+
+const createTables = (db) => {
+    // CHAMPION TABLES
+    db.query(
+        `CREATE TABLE IF NOT EXISTS champions (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(16) NOT NULL UNIQUE,
+            about TEXT
+    )`,
+    (err, result) => {
+        if (err) console.error("ERROR CREATING champion TABLE", err);
+        else console.log("CHAMPIONS TABLE CREATED");
+    }
+    );
+    // MATCHUP TABLES
+    db.query(
+        `CREATE TABLE IF NOT EXISTS matchups (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            playerId INT NOT NULL,
+            opponentId INT NOT NULL,
+            difficulty TINYINT UNSIGNED NOT NULL CHECK (difficulty BETWEEN 1 AND 9),
+            notes TEXT,
+            UNIQUE (playerId, opponentId),
+            FOREIGN KEY (playerId) REFERENCES champions(id),
+            FOREIGN KEY (opponentId) REFERENCES champions(id)
+    )`,
+    (err, result) => {
+        if (err) console.error("ERROR CREATING champion TABLE", err);
+        else console.log("MATCHUPS TABLE CREATED");
+    }
+    );
+}
 
 // app.use(express.static(__dirname));
 
