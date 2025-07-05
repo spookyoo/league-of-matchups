e2516241e216f25fbbda053201e67ec7172be29e
spookyoo <melvincruz07@gmail.com>
2025-07-05 17:41:16 -0600

    Created DatabasePage.js
    - Separated from App.js
    - Implemented routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,15 @@
-import React, { useEffect, useState} from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/DatabasePage";
 
 function App() {
-  const [champions, setChampions] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/champions")
-      .then(res => {
-        if (!res.ok) throw new Error("FAILED TO FETCH");
-        return res.json();
-      })
-      .then(data => setChampions(data))
-      .catch(err => {
-        console.error("ERROR FETCHING: ", err);
-        setError("Could not load champions");
-      });
-  }, []);
-
   return (
-    <div className="App">
-      <div className="navbar">
-        <h1>LEAGUE OF MATCHUPS</h1>
-      </div>
-      <div className="main"></div>
-        <h3>Choose a champion</h3>
-        <div className="championList">
-          {champions.map((champion) => (
-            <button key={champion.name} className="championButton">
-              {champion.name}
-            </button>
-          ))}
-        </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/DatabasePage.js
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState} from 'react';
+
+const DatabasePage = () => {
+    const navigate = useNavigate();
+    const [champions, setChampions] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/champions")
+        .then(res => {
+            if (!res.ok) throw new Error("FAILED TO FETCH");
+            return res.json();
+        })
+        .then(data => setChampions(data))
+        .catch(err => {
+            console.error("ERROR FETCHING: ", err);
+            setError("Could not load champions");
+        });
+    }, []);
+
+    return (
+    <div className="database-page">
+      <div className="navbar">
+        <h1>LEAGUE OF MATCHUPS</h1>
+      </div>
+      <div className="main"></div>
+        <h3>Choose a champion</h3>
+        <div className="championList">
+          {champions.map((champion) => (
+            <button key={champion.name} className="championButton">
+              {champion.name}
+            </button>
+          ))}
+        </div>
+    </div>
+    );
+};
+
+export default DatabasePage
